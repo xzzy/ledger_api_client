7f98a8877f6654c420b1aa090a4f54a0ee01e4d2
Jason Moore <jason@digitalreach.com.au>
2020-04-23 11:36:37 +0800

    Search API fixes

--- ledger_api_client/common.py
+++ ledger_api_client/common.py
@@ -14,12 +14,11 @@ def get_ledger_user_info_by_id(userid):
 
 
 def search_ledger_users(keyword):
-    print ("IN")
     json_response = {}
-    attributemap = {}
+    attributemap = {'keyword': keyword}
     data = urllib.parse.urlencode(attributemap)
     data = data.encode('utf-8')
-    with urllib.request.urlopen(settings.LEDGERGW_URL+"ledgergw/remote/user-search/"+keyword+"/"+settings.LEDGER_API_KEY+"/", data) as url:
+    with urllib.request.urlopen(settings.LEDGERGW_URL+"ledgergw/remote/user-search/"+settings.LEDGER_API_KEY+"/", data) as url:
            json_response = json.loads(url.read().decode().encode('utf-8'))
     return json_response
 
