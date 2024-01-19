babb605be2f12dd55ec2225d81dead3b9355674d
Jason Moore <jmoore@austwa.com>
2024-01-19 09:29:58 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='ledger_api_client',
-      version='1.28',
+      version='1.29',
       description='Ledger API Client',
       url='https://github.com/dbca-wa/ledger_api_client',
       author='Department of Parks and Wildlife',
