3bfef6169d513027368341fe1eb0b2982efa7e92
Jason Moore <jason.moore@dbca.wa.gov.au>
2024-08-04 13:50:25 +0800

    Version Increase

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='ledger_api_client',
-      version='1.35',
+      version='1.36',
       description='Ledger API Client',
       url='https://github.com/dbca-wa/ledger_api_client',
       author='Department of Biodiversity, Conservation and Attractions',
@@ -9,7 +9,7 @@ setup(name='ledger_api_client',
       license='BSD',
       packages=['ledger_api_client','ledger_api_client.migrations','ledger_api_client.management','ledger_api_client.management.commands',
                 ],
-      install_requires=[],
+      install_requires=['django-crispy-forms',],
       include_package_data=True,
       zip_safe=False)
 
