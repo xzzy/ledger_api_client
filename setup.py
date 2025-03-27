6324e51695a8da24590bf7f9946f506c7b5c9dae
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2025-03-27 10:18:30 +0800

    Update verison.

--- setup.py
+++ setup.py
@@ -1,22 +1,15 @@
 from setuptools import setup
 
-setup(
-    name="ledger_api_client",
-    version="1.69",
-    description="Ledger API Client",
-    url="https://github.com/dbca-wa/ledger_api_client",
-    author="Department of Biodiversity, Conservation and Attractions",
-    author_email="asi@dbca.wa.gov.au",
-    license="BSD",
-    packages=[
-        "ledger_api_client",
-        "ledger_api_client.migrations",
-        "ledger_api_client.management",
-        "ledger_api_client.management.commands",
-    ],
-    install_requires=[
-        "django-crispy-forms",
-    ],
-    include_package_data=True,
-    zip_safe=False,
-)
+setup(name='ledger_api_client',
+      version='1.69',
+      description='Ledger API Client',
+      url='https://github.com/dbca-wa/ledger_api_client',
+      author='Department of Biodiversity, Conservation and Attractions',
+      author_email='asi@dbca.wa.gov.au',
+      license='BSD',
+      packages=['ledger_api_client','ledger_api_client.migrations','ledger_api_client.management','ledger_api_client.management.commands',
+                ],
+      install_requires=['django-crispy-forms',],
+      include_package_data=True,
+      zip_safe=False)
+
