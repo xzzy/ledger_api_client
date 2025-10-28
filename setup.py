ef29654ae6f03caf8897fb9bdb59d1867df4f10e
Jason Moore <jmoore@austwa.com>
2025-10-28 14:23:08 +0800

    Bump version from 1.79 to 1.80

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='ledger_api_client',
-      version='1.79',
+      version='1.80',
       description='Ledger API Client',
       url='https://github.com/dbca-wa/ledger_api_client',
       author='Department of Biodiversity, Conservation and Attractions',
